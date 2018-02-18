873f95397aee81e56211802bb13b214306a43b39
Terryn <terryn007@Hotmail.com>
2018-02-18 09:50:52 -0500

    Intro project 1

---
+++ C++/Intro/Project1-Handout/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <random>
+#include <string>
+
+int GetRandom()
+{
+    std::srand(std::time(nullptr)); // use current time as seed for random generator
+    return std::rand();
+}
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+    return 0;
+}
